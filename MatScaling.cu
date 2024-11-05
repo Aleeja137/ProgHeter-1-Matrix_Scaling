d4118ac3519ee13dce1d033a867a5107a489e331
Alejandro Perez <perezalex137@gmail.com>
2024-11-05 21:00:25 +0100

    Parallel version complete
    Certain touchups are needed to better comply with statement
    requirements. Updated README

--- MatScaling.cu
+++ MatScaling.cu
@@ -5,6 +5,7 @@
 #include <math.h>
 #include <unistd.h> // For sleep
 #include <string.h> // Memcpy
+#include <cuda_runtime.h>
 
 // Defines 
 #define default_M 4000
@@ -54,6 +55,19 @@ void matrix_scaling_seq(float *mat, float *rMat, float *factors, int r, int m, i
 }
 
 // Define CUDA kernel
+__global__ void matrix_scaling_cuda(float *mat, float *rMat, float *factors, int r, int m, int n){
+
+    int global_thread_id = blockIdx.x * blockDim.x + threadIdx.x;
+
+    int rep;
+
+    if (global_thread_id < m*n)
+    {
+        rMat[global_thread_id] = mat[global_thread_id] * factors[0];
+        for (rep=1; rep<r; rep++) rMat[global_thread_id] *= factors[rep];
+    }
+
+}
 
 // Main
 int main(int argc, char *argv[])
@@ -62,7 +76,11 @@ int main(int argc, char *argv[])
     // int i;
     int m, n, r, threads_blk; 
     float *mat, *matSeq, *matPar, *factors;
-    float *mat_cuda, *matResults_cuda;
+    float *mat_cuda, *matResults_cuda, *factors_cuda;
+    cudaEvent_t start_seq, start_par, start_par_tot, end_seq, end_par;
+    cudaEventCreate(&start_seq); cudaEventCreate(&end_seq);
+    cudaEventCreate(&start_par); cudaEventCreate(&end_par);
+    cudaEventCreate(&start_par_tot);
 
     m = default_M;
     n = default_N;
@@ -75,7 +93,7 @@ int main(int argc, char *argv[])
     if (argc > 3) r = atoi(argv[3]);
     if (argc > 4) threads_blk = atoi(argv[4]);
     if (argc > 5) {fprintf(stderr, "Usage: %s nrows(m:4000) ncol(n:4000) repetitions(r:5) threads_per_block(threads_blk:32)\n", argv[0] ); exit(1);}
-    if (threads_blk != 32 && threads_blk != 64 && threads_blk != 128) {fprintf(stderr, "Correct threads_blk: {32,64,128}\n", argv[0] ); exit(1);}
+    if (threads_blk != 32 && threads_blk != 64 && threads_blk != 128) {fprintf(stderr, "Correct threads_blk: {32,64,128}\n"); exit(1);}
 
     printf("\n ---Program start---\n\n Configuration chosen --> m: %d, n: %d, r: %d, threads_blk: %d\n",m,n,r,threads_blk);
     srand(42); // Meaning of life
@@ -93,16 +111,22 @@ int main(int argc, char *argv[])
     populate_matrix(factors, r);
 
     // Get sequential result for later correctness analysis
+    printf("Starting sequential matrix scaling ... \n");
+    cudaEventRecord(start_seq);
     matrix_scaling_seq(mat, matSeq, factors, r, m, n);
+    cudaEventRecord(end_seq);
 
     // Allocate memory on CUDA device
+    cudaEventRecord(start_par_tot);
     unsigned int numBytes = m*n*sizeof(float);
-    cudaMalloc((void **) &mat_cuda , numBytes) ;
-    cudaMalloc((void **) &matResults_cuda , numBytes) ;
+    cudaMalloc((void **) &mat_cuda , numBytes);
+    cudaMalloc((void **) &matResults_cuda , numBytes);
+    cudaMalloc((void **) &factors_cuda, r*sizeof(float));
 
     // Pass data to CUDA device memory
     cudaMemcpy(mat_cuda,mat,numBytes,cudaMemcpyHostToDevice);
     cudaMemset(matResults_cuda, 0, numBytes);
+    cudaMemcpy(factors_cuda, factors,r*sizeof(float),cudaMemcpyHostToDevice);
 
     // Calculate kernel call dimensions
     unsigned int tasks = m*n;
@@ -110,16 +134,37 @@ int main(int argc, char *argv[])
     dim3 dimGrid((tasks+dimBlock.x-1)/dimBlock.x);  // tasks/threads_blk blocks, rounded if not divisible
     
     // Call kernel
-    // Synchronize (necessary?)
+    printf("Starting parallel matrix scaling ... \n");
+    cudaEventRecord(start_par);
+    matrix_scaling_cuda<<<dimGrid, dimBlock>>>(mat_cuda, matResults_cuda, factors_cuda, r, m, n);
+    cudaEventRecord(end_par);
+
     // Receive data from CUDA device
+    cudaMemcpy(matPar, matResults_cuda, numBytes, cudaMemcpyDeviceToHost); 
+
     // Free memory on CUDA device
+    cudaFree(mat_cuda);
+    cudaFree(matResults_cuda);
+    cudaFree(factors_cuda);
 
     // Check results
-    int equal = mat_equal(mat, mat, m*n, -1);
+    int equal = mat_equal(matSeq, matPar, m*n, -1);
     if (equal) printf("Results are OK :)\n");
     else       printf("Results are NOT ok :'(\n");
 
+    float time_seq, time_par, time_par_tot;
+    cudaEventElapsedTime(&time_seq, start_seq, end_seq);
+    cudaEventElapsedTime(&time_par, start_par, end_par);
+    cudaEventElapsedTime(&time_par_tot, start_par_tot, end_par);
+
+    printf("Time for sequential:                       %.3f ms\n",time_seq);
+    printf("Time for parallel (computation only):      %.3f ms\n",time_par);
+    printf("Time for parallel (memory management too): %.3f ms\n",time_par_tot);
+
     // Frees
+    cudaEventDestroy(start_seq); cudaEventDestroy(end_seq);
+    cudaEventDestroy(start_par); cudaEventDestroy(end_par);
+    cudaEventDestroy(start_par_tot);
     free(factors);
     free(mat);
     free(matSeq);
--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-Readme test
+# Heterogeneous Programming - Deliverable 1 - Matrix Scaling on GPU  
+This code compared the performance of matrix scaling on CPU and GPU. It is intended to run on Finis Terrae 3 supercomputer using 1 A100 Nvidia GPU.  
+  
+For this deliverable, matrix scaling is performed iteratively multiplying (scaling) a matrix's elements with a factor between 0.0 and 1.0 .  
+
+This is the first graded deliverable for the Heterogeneous Programming subject of the HPC Master at UDC.  
+  
+## Compiling  
+For compilation, it is enough to execute the command:  
+`nvcc -o mat MatScaling.cu -lm -Xcompiler -Wall -Xcompiler -Wextra`   
+
+## Executing  
+For executing, it can be done directly on an interactive GPU node (for testing) or using the slurm bash submission script included in the repository (pending):    
+`sbatch submit_script.sh`  
+
+The program takes up to 4 arguments, which are in order `m n r threads_blk`:  
+- m represents the number of rows of the matrix  
+- n represents the number of columns of the matrix  
+- r represents the number of scaling factors used  
+- threads_blk represents the number of threads per block when calling the CUDA kernell. Only accepted values are {32,64,128}  
+
+By default, `m=4000`, `n=4000`, `r=5` and `threads_blk=32`.  
+  
+## Examples
+When running the program with default settings, the following output is shown (speed-up pending):  
+
+```
+ ---Program start---
+
+ Configuration chosen --> m: 4000, n: 4000, r: 5, threads_blk: 32
+Starting sequential matrix scaling ... 
+Starting parallel matrix scaling ... 
+Results are OK :)
+Time for sequential:                       243.376 ms
+Time for parallel (computation only):      1.093 ms
+Time for parallel (memory management too): 8.264 ms
+```
